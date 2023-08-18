6c7c33430e71a023525b6dbbdaacd02872ce0848
M2i_Beffroi <mon.email@exemple.fr>
2023-08-18 15:04:09 +0200

    save2ExoBanque

--- exo_DAO_Banque/src/main/java/org/example/dao/ClientDAO.java
+++ exo_DAO_Banque/src/main/java/org/example/dao/ClientDAO.java
@@ -4,6 +4,7 @@ import org.example.model.Client;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 
@@ -14,11 +15,19 @@ public class ClientDAO extends BaseDAO<Client> {
 
     @Override
     public boolean save(Client element) throws SQLException {
-
-
-
-        return false;
+        request = "INSERT INTO client (first_name, last_name, numberPhone) VALUES (?,?,?)";
+        statement = _connection.prepareStatement(request, Statement.RETURN_GENERATED_KEYS);
+        statement.setString(1, element.getFirst_name());
+        statement.setString(2, element.getLast_name());
+        statement.setString(3,element.getNumberPhone());
+        int nbRows = statement.executeUpdate();
+        resultSet = statement.getGeneratedKeys();
+        if(resultSet.next()){
+            element.setId(resultSet.getInt(1));
+        }
+        return nbRows == 1;
     }
+
 
     @Override
     public boolean update(Client element) throws SQLException {
